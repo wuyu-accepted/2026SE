d00d31c7c3e41711dc80c4c6ae85d6234c6cf435
Hash012 <2024201513@ruc.edu.cn>
2026-05-26 20:00:39 +0800

    一些小修复

--- miniprogram-1/utils/config.js
+++ miniprogram-1/utils/config.js
@@ -1,10 +1,12 @@
 const BASE_URL = 'http://10.10.0.6'
+const REQUEST_TIMEOUT = 10000
 const TOKEN_KEY = 'accessToken'
 const USER_KEY = 'currentUser'
 const LOGIN_PAGE = '/pages/login/login'
 
 module.exports = {
   BASE_URL,
+  REQUEST_TIMEOUT,
   TOKEN_KEY,
   USER_KEY,
   LOGIN_PAGE,
